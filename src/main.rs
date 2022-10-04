7425e0020ac9111e36630a32c593fff63d363255
Francis Beeson <leobeeson@gmail.com>
2022-10-04 16:55:14 +0100

    Add rendering of game frame

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{
     error::Error, 
     io,
-    time::Duration,
+    time::Duration, sync::mpsc, thread,
 };
 use crossterm::{
     terminal::{
@@ -18,6 +18,7 @@ use crossterm::{
         self
     }
 };
+use invaders::{frame::{self, new_frame}, render};
 use rusty_audio::Audio;
 
 
@@ -37,8 +38,26 @@ fn main() -> Result <(), Box<dyn Error>> {
     stdout.execute(EnterAlternateScreen)?;
     stdout.execute(Hide)?;
 
+    // Render loop in a separate thread
+    let (render_tx, render_rx) = mpsc::channel();
+    let render_handle = thread::spawn(move || {
+        let mut last_frame = frame::new_frame();
+        let mut stdout = io::stdout();
+        render::render(&mut stdout, &last_frame, &last_frame, true);
+        loop {
+            let curr_frame = match render_rx.recv() {
+                Ok(x) => x,
+                Err(_) => break,
+            };
+            render::render(&mut stdout, &last_frame, &curr_frame, false);
+            last_frame = curr_frame;
+        }
+    });
+
     // Game Loop
     'gameloop: loop {
+        // Per-frame init
+        let curr_frame = new_frame();
         // Input
         while event::poll(Duration::default())? {
             if let Event::Key(key_event) = event::read()? {
@@ -51,9 +70,14 @@ fn main() -> Result <(), Box<dyn Error>> {
                 }
             }
         }
+        // Draw & Render
+        let _ = render_tx.send(curr_frame);
+        thread::sleep(Duration::from_millis(1));
     }
 
     // Cleanup
+    drop(render_tx);
+    render_handle.join().unwrap();
     audio.wait();
     stdout.execute(Show)?;
     stdout.execute(LeaveAlternateScreen)?;
